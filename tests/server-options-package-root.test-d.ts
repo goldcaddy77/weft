a06b1b082bb3371c2194a8a7eb9a05f80066efc2
Steve Kinney <hello@stevekinney.net>
2026-05-13 06:33:14 -0600

    test: cover package metrics option removal

---
+++ tests/server-options-package-root.test-d.ts
@@ -0,0 +1,28 @@
+import { createMetricsCollectorExporter, Engine } from 'weft';
+import type { ServeOptions } from 'weft/server';
+import type { HandlerOptions } from 'weft/server/handler';
+
+const packageRootEngine = new Engine();
+const packageRootPrometheusExporter = createMetricsCollectorExporter(undefined);
+
+const packageRootServeOptions: ServeOptions = {
+  engine: packageRootEngine,
+  prometheusExporter: packageRootPrometheusExporter,
+};
+void packageRootServeOptions;
+
+const legacyPackageRootServeOptions: ServeOptions = {
+  engine: packageRootEngine,
+  // @ts-expect-error `metricsCollector` is no longer a public package server option.
+  metricsCollector: undefined,
+};
+void legacyPackageRootServeOptions;
+
+const packageRootHandlerOptions: HandlerOptions = {
+  prometheusExporter: packageRootPrometheusExporter,
+};
+void packageRootHandlerOptions;
+
+// @ts-expect-error `metricsCollector` is no longer a public package handler option.
+const legacyPackageRootHandlerOptions: HandlerOptions = { metricsCollector: undefined };
+void legacyPackageRootHandlerOptions;
