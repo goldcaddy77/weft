d0d22f2ce5dbd7eb4a51ed8dbc17f1001e442b12
Steve Kinney <hello@stevekinney.net>
2026-05-18 10:06:52 -0600

    chore(examples): re-format order-processing per prettier

--- examples/order-processing/src/messages.ts
+++ examples/order-processing/src/messages.ts
@@ -1,11 +1,6 @@
 import { query, signal, update } from 'weft';
 
-import type {
-  AddItemInput,
-  AddItemResult,
-  CancelOrderInput,
-  OrderStatus,
-} from './model';
+import type { AddItemInput, AddItemResult, CancelOrderInput, OrderStatus } from './model';
 
 export const addItemUpdate = update<AddItemInput, AddItemResult>('addItem');
 export const cancelOrderSignal = signal<CancelOrderInput>('cancelOrder');
